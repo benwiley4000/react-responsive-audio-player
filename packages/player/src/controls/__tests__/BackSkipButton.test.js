79122f5c05c36e66f3dce85db413b9df81d016ba
Ben Wiley <therealbenwiley@gmail.com>
2018-12-16 20:22:00 -0500

    Back skip button test fixed.

--- packages/player/src/controls/__tests__/BackSkipButton.test.js
+++ packages/player/src/controls/__tests__/BackSkipButton.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import BackSkipButton from '../BackSkipButton';
 import renderer from 'react-test-renderer';
 
-test('Control renders correctly', () => {
+import PlayerContext from '@cassette/core/src/PlayerContext';
+
+import BackSkipButton from '../BackSkipButton';
+
+const mockPlayerContext = {
+  onBackSkip: () => null
+};
+
+test('Back skip button renders correctly', () => {
   const tree = renderer
-    .create(<BackSkipButton onBackSkip={() => null} />)
+    .create(
+      <PlayerContext.Provider value={mockPlayerContext}>
+        <BackSkipButton />
+      </PlayerContext.Provider>
+    )
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
